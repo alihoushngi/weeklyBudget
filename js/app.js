108e07b666817d0b2da7271a43ba9113a2c6a4f5
alihoushngi <62546675+alihoushngi@users.noreply.github.com>
2022-02-28 17:00:18 +0330

    2-28-5:00

--- js/app.js
+++ js/app.js
@@ -1,7 +1,49 @@
 //class
+class Budget {
+  constructor(budget) {
+    this.budget = budget;
+  }
+}
 
+class HTML {
+  insertBudget(amount) {
+    budgetTotal.innerHTML = amount;
+    budgetLeft.innerHTML = amount;
+  }
+
+  printMessage(message, className) {
+    const div = document.createElement("div");
+    div.classList.add("alert", "text-center", className);
+    div.textContent = message;
+    const primary = document.querySelector(".primary");
+    primary.insertBefore(div, addExpenseForm);
+
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 2000);
+    addExpenseForm.reset();
+  }
+
+  insertExpensList(name, amount) {
+    const expenses = document.querySelector("#expenses ul");
+
+    let li = document.createElement("li");
+    li.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = `
+    ${name}
+   <span class = "badge badge-primary  badge-pill ">${amount}</span> 
+    `;
+    expenses.appendChild(li);
+  }
+}
 //variable
 let userBudget;
+let budget;
+let budgetTotal = document.querySelector("span#total");
+let budgetLeft = document.querySelector("span#left");
+const html = new HTML();
+const addExpenseForm = document.querySelector("#add-expense");
 //eventlisteners
 eventListeners();
 function eventListeners() {
@@ -11,6 +53,21 @@ function eventListeners() {
     //validate user budget
     if (userBudget === null || userBudget === "" || userBudget === "0") {
       window.location.reload();
+    } else {
+      budget = new Budget(userBudget);
+      html.insertBudget(userBudget);
+    }
+  });
+
+  addExpenseForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let expense = document.querySelector("#expense").value;
+    let amount = document.querySelector("#amount").value;
+
+    if (expense === "" || amount === "") {
+      html.printMessage("همه موارد الزامی است", "alert-danger");
+    } else {
+      html.insertExpensList(expense, amount);
     }
   });
 }
